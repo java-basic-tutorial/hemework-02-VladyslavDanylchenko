c0cf43191c11e636022caeeb0ee955ebd0e66be8
VladyslavDanylchenko <98156931+VladyslavDanylchenko@users.noreply.github.com>
2022-01-24 15:18:41 +0200

    Completed task03 of homework02

--- src/main/java/com/softserveinc/Task03.java
+++ src/main/java/com/softserveinc/Task03.java
@@ -1,5 +1,6 @@
 package com.softserveinc;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -24,5 +25,27 @@ public class Task03 {
         Scanner scanner = new Scanner(System.in);
         // TODO: Write your code here
 
+        int n;
+        int[] arr = new int[0];
+        do {
+            System.out.print(">>> ");
+            n = scanner.nextInt();
+            if (n <= 0)
+                break;
+            arr = Arrays.copyOf(arr, arr.length + 1);
+            arr[arr.length - 1] = n;
+        } while (true);
+
+        if (arr.length != 0) {
+            int min = 0;
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] < arr[min]) {
+                    min = i;
+                }
+            }
+            System.out.printf("Minimum number is %d\n", arr[min]);
+        } else {
+            System.out.println("None of valid numbers entered");
+        }
     }
 }
